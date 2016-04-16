3f26727453573746e90273060062b9817fd06ea7
attakei <attakei@gmail.com>
2016-04-16 22:50:19 +0900

    Refactor: fix code

--- lantis/webradio/commands/fetch_channels.py
+++ lantis/webradio/commands/fetch_channels.py
@@ -1,12 +1,13 @@
 # -*- coding:utf8 -*-
+import sys
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
 from .. import INDEX_URL, Channel
 
 
-def main():
+def main(out=sys.stdout, err=sys.stderr):
     """Fetch current chennels in lantis-net.com
     """
-    from urllib.request import urlopen, Request
-    from bs4 import BeautifulSoup
     req = Request(INDEX_URL)
     req.add_header('User-agent', 'Macintosh')
     resp = urlopen(req)
@@ -14,5 +15,5 @@ def main():
     channels = soup.find_all('div', class_='titles')
     for channel_ in channels:
         channel = Channel.from_html(channel_)
-        print(channel.channel_id, ':')
-        print('\t', channel.title)
+        out.write(channel.channel_id + ' :\n')
+        out.write('\t' + channel.title + '\n')
